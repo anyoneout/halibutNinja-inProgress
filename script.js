e2148113c3d22308ecee3b86b7f9dc227c5ebe00
anyoneout <chrisdafur@gmail.com>
2024-06-23 12:40:03 -0500

    started

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
-const textElement = document.getElementById('text')
-const optionsButtonsElement = document.getElementById('option-buttons')
+function adventureGame () {
+    let characterName;
+    let characterClass;
+    let inventory;
+    let city;
+    let street;
+    let var6;
 
-let state = {}
-
-
-function startGame() {
-    state = {}
-    showTextNode(1)
+    characterName = prompt("What is your character's name?");
+    characterClass = prompt(`Hi ${characterName}, what class adventurer are you? (Fighter / Mage / Halibut Ninja)`);
+        if (characterClass = "Halibut Ninja") {
+            inventory.sword = "Halibut Sword";
+            inventory.shield = "Halibut Shield";
+            inventory.armor = "Halibut Armor";
+            }
+        else if (characterClass = "Fighter") {
+            inventoryHeader.innerHTML = "You have died";
+        }
+        else inventoryHeader.innerHTML = "Upon opening your first book of magic you give yourself a magic papercut and get a magic infection and magically die.";
+   
 }
+adventureGame();
 
-function showTextNode(textNodeIndex) {
-    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-}
-
-function selectOption(option) {
-
-}
-
-const textNodes = [
-    {
-        id: 1,
-        text: 'You wake up on Hendrix street with a new day before you.', 
-        options: [
-            {
-                text: 'Go to the pool',
-                nextText: 2
-            },
-            {
-                text: 'Go to the arcade',
-                nextText: 2
-            },  
-            {
-                text: 'Go on a savage hunt',
-                nextText: 3
-            },  
-        ]
-    }, 
-    {
-        id: 2
-    }
-]
-
-startGame() 
